e014c3626a91076977da7a0abb6b8b0afb409356
Will Gorman <wgorman@pentaho.com>
2007-08-15 15:27:49 +0000

    upgraded to the latest version of pentaho meta jar BISERVER-136

--- src/org/pentaho/commons/mql/ui/mqldesigner/MQLDemo.java
+++ src/org/pentaho/commons/mql/ui/mqldesigner/MQLDemo.java
@@ -5,13 +5,14 @@ import org.eclipse.swt.widgets.Shell;
 
 public class MQLDemo {
   public static void main(String args[]) {
-    CWMStartup.loadCWMInstance("resources/metadata/repository.properties", "resources/metadata/PentahoCWM.xml");
-    CWMStartup.loadMetadata("resources/metadata.xmi", "resources");
+    CWMStartup.loadCWMInstance("resources/metadata/repository.properties", "resources/metadata/PentahoCWM.xml"); //$NON-NLS-1$ //$NON-NLS-2$
+    CWMStartup.loadMetadata("resources/metadata.xmi", "resources"); //$NON-NLS-1$ //$NON-NLS-2$
     Shell shell = new Shell();
     MQLQueryBuilderDialog dialog = new MQLQueryBuilderDialog(shell);
     if (dialog.open() == Window.OK) {
       try {
-        System.out.println("mqlQuery.getXML() = " + dialog.getMqlQuery().getXML());
+        System.out.println("mqlQuery.getXML() = \n" + dialog.getMqlQuery().getXML()); //$NON-NLS-1$
+        System.out.println("\nmqlQuery.getQuery() = \n" + dialog.getMqlQuery().getQuery(true)); //$NON-NLS-1$
       } catch (Exception e) {
         e.printStackTrace();
       }
