cb558e75f356c4409056d3995e9fccc4f6f326dd
Gretchen Moran <gmoran@pentaho.com>
2007-08-28 19:05:35 +0000

    PMD-175 refactor fix for length of column identifiers

--- src/org/pentaho/commons/mql/ui/mqldesigner/MQLDemo.java
+++ src/org/pentaho/commons/mql/ui/mqldesigner/MQLDemo.java
@@ -1,7 +1,12 @@
 package org.pentaho.commons.mql.ui.mqldesigner;
 
+import java.io.IOException;
+
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
+import org.pentaho.pms.core.exception.PentahoMetadataException;
+import org.pentaho.pms.factory.CwmSchemaFactory;
+import org.pentaho.pms.mql.MQLQuery;
 
 public class MQLDemo {
   public static void main(String args[]) {
@@ -12,7 +17,8 @@ public class MQLDemo {
     if (dialog.open() == Window.OK) {
       try {
         System.out.println("mqlQuery.getXML() = \n" + dialog.getMqlQuery().getXML()); //$NON-NLS-1$
-        System.out.println("\nmqlQuery.getQuery() = \n" + dialog.getMqlQuery().getQuery(true)); //$NON-NLS-1$
+        System.out.println("\nmqlQuery.getQuery() = \n" + dialog.getMqlQuery().getQuery().getQuery()); //$NON-NLS-1$
+        System.out.println("\nmqlQuery.getQuery().getDisplayQuery() = \n" + dialog.getMqlQuery().getQuery().getDisplayQuery()); //$NON-NLS-1$
       } catch (Exception e) {
         e.printStackTrace();
       }
