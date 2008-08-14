19f5a2f1dadea4e8d201a565a772431a10f2e3f9
William Seyler <wseyler@pentaho.com>
2008-08-14 15:17:18 +0000

    BISERVER-1626: Code complete for schedule panel.

--- pentaho-gwt-widgets/source/org/pentaho/gwt/widgets/client/wizards/panels/ScheduleEditorWizardPanel.java
+++ pentaho-gwt-widgets/source/org/pentaho/gwt/widgets/client/wizards/panels/ScheduleEditorWizardPanel.java
@@ -17,24 +17,22 @@
 
 package org.pentaho.gwt.widgets.client.wizards.panels;
 
-import java.util.Map;
+import java.util.Date;
 
 import org.pentaho.gwt.widgets.client.controls.schededitor.ScheduleEditor;
-import org.pentaho.gwt.widgets.client.utils.TimeUtil.DayOfWeek;
+import org.pentaho.gwt.widgets.client.controls.schededitor.ScheduleEditor.ScheduleType;
+import org.pentaho.gwt.widgets.client.ui.ICallback;
+import org.pentaho.gwt.widgets.client.ui.IChangeHandler;
 import org.pentaho.gwt.widgets.client.wizards.AbstractWizardPanel;
 import org.pentaho.gwt.widgets.client.wizards.panels.validators.ScheduleEditorValidator;
 
-import com.google.gwt.user.client.ui.ChangeListener;
-import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.ClickListener;
-import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author wseyler
  *
  */
-public class ScheduleEditorWizardPanel extends AbstractWizardPanel implements KeyboardListener, ClickListener, ChangeListener {
+public class ScheduleEditorWizardPanel extends AbstractWizardPanel {
 
   ScheduleEditor scheduleEditor = new ScheduleEditor();
   ScheduleEditorValidator scheduleEditorValidator;
@@ -50,43 +48,12 @@ public class ScheduleEditorWizardPanel extends AbstractWizardPanel implements Ke
    * 
    */
   private void init() {
-    scheduleEditor.getCronEditor().getCronTb().addKeyboardListener(this);
-    scheduleEditor.getCronEditor().getDateRangeEditor().getEndDatePanel().getEndByRb().addClickListener(this);
-    scheduleEditor.getDescriptionTb().addKeyboardListener(this);
-    scheduleEditor.getGroupNameTb().addKeyboardListener(this);
-    scheduleEditor.getNameTb().addKeyboardListener(this);
-    scheduleEditor.getScheduleCombo().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getDailyEditor().getEveryNDaysRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getDailyEditor().getEveryWeekdayRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getDailyEditor().getRepeatValueTb().addKeyboardListener(this);
-    scheduleEditor.getRecurrenceEditor().getDateRangeEditor().getEndDatePanel().getEndByRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getDateRangeEditor().getEndDatePanel().getEndDatePicker().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getDateRangeEditor().getEndDatePanel().getNoEndDateRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getDateRangeEditor().getStartDatePicker().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getHourlyEditor().getValueTb().addKeyboardListener(this);
-    scheduleEditor.getRecurrenceEditor().getMinutelyEditor().getValueTb().addKeyboardListener(this);
-    scheduleEditor.getRecurrenceEditor().getMonthlyEditor().getDayNOfMonthRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getMonthlyEditor().getDayOfMonthTb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getMonthlyEditor().getDayOfWeekLb().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getMonthlyEditor().getNthDayNameOfMonthRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getMinutelyEditor().getValueTb().addKeyboardListener(this);
-    scheduleEditor.getRecurrenceEditor().getMonthlyEditor().getWhichWeekLb().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getSecondlyEditor().getValueTb().addKeyboardListener(this);
-    Map<DayOfWeek, CheckBox> checkBoxMap = scheduleEditor.getRecurrenceEditor().getWeeklyEditor().getDayToCheckBox();
-    for (DayOfWeek dayOfWeek : checkBoxMap.keySet()) {
-      checkBoxMap.get(dayOfWeek).addClickListener(this);
-    }
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getDayOfMonthTb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getDayOfWeekLb().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getEveryMonthOnNthDayRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getMonthOfYearLb0().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getMonthOfYearLb1().addChangeListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getNthDayNameOfMonthNameRb().addClickListener(this);
-    scheduleEditor.getRecurrenceEditor().getYearlyEditor().getWhichWeekLb().addChangeListener(this);
-    scheduleEditor.getRunOnceEditor().getStartDatePicker().addChangeListener(this);
-    scheduleEditor.getRunOnceEditor().getStartTimePicker().getHourLB().addChangeListener(this);
-    scheduleEditor.getRunOnceEditor().getStartTimePicker().getMinuteLB().addChangeListener(this);
-    scheduleEditor.getRunOnceEditor().getStartTimePicker().getTimeOfDayLB().addChangeListener(this);
+    ICallback<IChangeHandler> chHandler = new ICallback<IChangeHandler>() {
+      public void onHandle(IChangeHandler se ) {
+        panelWidgetChanged(ScheduleEditorWizardPanel.this);
+      }
+    };
+    scheduleEditor.setOnChangeHandler( chHandler );
   }
 
   /**
@@ -105,10 +72,14 @@ public class ScheduleEditorWizardPanel extends AbstractWizardPanel implements Ke
     return "Schedule Edit";
   }
 
-  private void panelWidgetChanged(Widget changedWidget) {
-    System.out.println("Widget Changed: " + changedWidget + " can continue: " + scheduleEditorValidator.isValid());
+  protected void panelWidgetChanged(Widget changedWidget) {
+//    System.out.println("Widget Changed: " + changedWidget + " can continue: " + scheduleEditorValidator.isValid());
     this.setCanContinue(scheduleEditorValidator.isValid());
     this.setCanFinish(scheduleEditorValidator.isValid());
+  }
+  
+  public ScheduleType getScheduleType() {
+    return scheduleEditor.getScheduleType();
   }
   
   /**
@@ -118,51 +89,32 @@ public class ScheduleEditorWizardPanel extends AbstractWizardPanel implements Ke
     return scheduleEditor.getCronString();
   }
   
-  public String getScheduleName() {
+  public String getTriggerName() {
     return scheduleEditor.getName();
+  }
+  
+  public String getTriggerGroup() {
+    return scheduleEditor.getGroupName();
   }
   
   public String getDescription() {
     return scheduleEditor.getDescription();
   }
 
-  // KeyboardListener Stuff
-  /* (non-Javadoc)
-   * @see com.google.gwt.user.client.ui.KeyboardListener#onKeyDown(com.google.gwt.user.client.ui.Widget, char, int)
-   */
-  public void onKeyDown(Widget sender, char keyCode, int modifiers) {
-    // noop   
-  }
-
-  /* (non-Javadoc)
-   * @see com.google.gwt.user.client.ui.KeyboardListener#onKeyPress(com.google.gwt.user.client.ui.Widget, char, int)
-   */
-  public void onKeyPress(Widget sender, char keyCode, int modifiers) {
-    // noop
-  }
-
-  /* (non-Javadoc)
-   * @see com.google.gwt.user.client.ui.KeyboardListener#onKeyUp(com.google.gwt.user.client.ui.Widget, char, int)
-   */
-  public void onKeyUp(Widget sender, char keyCode, int modifiers) {
-    panelWidgetChanged(sender);
-  }
-
-  // ClickListener Stuff
-  /* (non-Javadoc)
-   * @see com.google.gwt.user.client.ui.ClickListener#onClick(com.google.gwt.user.client.ui.Widget)
-   */
-  public void onClick(Widget sender) {
-    panelWidgetChanged(sender);
+  public Date getStartDate() {
+    return scheduleEditor.getStartDate();
   }
   
+  public Date getEndDate() {
+    return scheduleEditor.getEndDate();
+  }
   
-  // ChangeListener Stuff
-  /* (non-Javadoc)
-   * @see com.google.gwt.user.client.ui.ChangeListener#onChange(com.google.gwt.user.client.ui.Widget)
-   */
-  public void onChange(Widget sender) {
-    panelWidgetChanged(sender);
+  public int getRepeatCount() {
+    // Repeate forever
+    return -1;
+  }
+  public String getRepeatInterval() {
+    return scheduleEditor.getRepeatInSecs().toString();
   }
   
 }
