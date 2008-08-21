56e862eba0620867cd28827de117b9455e385d6f
William Seyler <wseyler@pentaho.com>
2008-08-21 20:55:23 +0000

    Initial check in of toolbar stuff

---
+++ pentaho-gwt-widgets/source/org/pentaho/gwt/widgets/client/toolbar/ToolbarComboButton.java
@@ -0,0 +1,117 @@
+package org.pentaho.gwt.widgets.client.toolbar;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MouseListener;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class ToolbarComboButton extends ToolbarButton{
+  
+  private String COMBO_STYLE = "toolbar-combo-button";   //$NON-NLS-1$
+  private MenuBar menu;
+  
+  /**
+   * Constructs a toolbar button with an image and a label
+   * 
+   * @param img GWT Image object 
+   * @param label String containing an option label
+   */
+  public ToolbarComboButton(Image img, String label){
+    super(img, label);
+    addDropdownControl();
+    super.setStylePrimaryName(COMBO_STYLE);
+  }
+  
+  /**
+   * Constructs a toolbar button with an enabled image, disabled image and a label
+   * 
+   * @param img GWT Image object 
+   * @param disabledImage GWT Image object 
+   * @param label String containing an option label
+   */
+  public ToolbarComboButton(Image img, Image disabledImage, String label){
+    super(img, disabledImage, label);
+    addDropdownControl();
+    super.setStylePrimaryName(COMBO_STYLE);
+  }
+  
+
+  /**
+   * Constructs a toolbar button with an enabled image, disabled image and a label
+   * 
+   * @param img GWT Image object 
+   * @param disabledImage GWT Image object 
+   * @param label String containing an option label
+   */
+  public ToolbarComboButton(Image img, Image disabledImage){
+    super(img, disabledImage);
+    addDropdownControl();
+    super.setStylePrimaryName(COMBO_STYLE);
+  }
+  
+  /**
+   * Constructs a toolbar button with an image
+   * 
+   * @param img GWT Image object 
+   */
+  public ToolbarComboButton(Image img){
+    super(img);
+    addDropdownControl();
+    super.setStylePrimaryName(COMBO_STYLE);
+  }
+  
+  private void addDropdownControl(){
+    
+    
+  }
+  
+  @Override
+  public void setCommand(Command cmd) {
+    throw new UnsupportedOperationException("Not implemented in this class");   //$NON-NLS-1$
+  }
+  
+  private PopupPanel createPopup(){
+    
+    PopupPanel popup = new PopupPanel(true);
+    popup.setWidget(menu);
+    return popup;
+  }
+  
+  @Override
+  protected void addStyleMouseListener(){
+    eventWrapper.addMouseListener(new MouseListener(){
+      private PopupPanel popup;
+      public void onMouseDown(Widget w, int x, int y) {
+        if(!enabled){
+          return;
+        }
+        if(this.popup != null){
+          popup.hide();
+          popup.removeFromParent();
+        }
+        
+        this.popup = createPopup();
+        
+        popup.setPopupPosition(w.getAbsoluteLeft(), w.getAbsoluteTop() + w.getOffsetHeight());
+        popup.show();
+      }
+      public void onMouseEnter(Widget w) {
+        button.addStyleName(stylePrimaryName+"-hovering");    //$NON-NLS-1$
+      }
+      public void onMouseLeave(Widget w) {
+        button.removeStyleName(stylePrimaryName+"-hovering");   //$NON-NLS-1$
+      }
+      public void onMouseUp(Widget w, int x, int y) {
+      }
+      public void onMouseMove(Widget w, int x, int y) {}
+    });
+  }
+  
+  public void setMenu(MenuBar bar){
+    this.menu = bar;
+  }
+}
