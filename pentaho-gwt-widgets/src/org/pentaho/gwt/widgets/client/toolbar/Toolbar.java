a862f41f1fd62bd888fdea7e9ba72c57c489ad75
William Seyler <wseyler@pentaho.com>
2008-08-26 21:01:06 +0000

    Fixed bug with enabling menus where a disabled combo button could still show a menu.

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/toolbar/Toolbar.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/toolbar/Toolbar.java
@@ -102,11 +102,13 @@ public class Toolbar extends HorizontalPanel{
   public void setEnabled(boolean enabled){
     try{
       for(ToolbarButton button : this.buttons){
+        button.setEnabled(enabled);
         button.setTempDisabled(!enabled);
       }
       
       for(ToolbarGroup gp : groups){
-       gp.setTempDisabled(!enabled);
+        gp.setEnabled(enabled);
+        gp.setTempDisabled(!enabled);
       }
       
     } catch(Exception e){
