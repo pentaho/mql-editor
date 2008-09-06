7040d72e934f4e801872568b51bca56a559d808d
Davide Kincade <dkincade@pentaho.com>
2008-09-06 20:49:12 +0000

    Adding a trunk folder for gwt projects

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/ui/ICallback.java
+++
@@ -1,6 +0,0 @@
-package org.pentaho.gwt.widgets.client.ui;
-
-public interface ICallback<T> {
-
-  public void onHandle( T o );
-}
--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/ui/IChangeHandler.java
+++
@@ -1,5 +0,0 @@
-package org.pentaho.gwt.widgets.client.ui;
-
-public interface IChangeHandler {
-  public void setOnChangeHandler( ICallback<IChangeHandler> handler );
-}
