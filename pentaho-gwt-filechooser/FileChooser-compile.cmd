79d3cd16aa9c67462255af3c56a2d377468093ac
Davide Kincade <dkincade@pentaho.com>
2008-09-08 15:31:05 +0000

    Removing gwt filechooser after merge to gwt-widgets

--- pentaho-gwt-filechooser/FileChooser-compile.cmd
+++
@@ -1,1 +0,0 @@
-@java -Xmx256M -cp "%~dp0\src;%~dp0\bin;C:/Documents and Settings/Michael D'Amour/Desktop/gwt-windows-1.5.0/gwt-user.jar;C:/Documents and Settings/Michael D'Amour/Desktop/gwt-windows-1.5.0/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\www" %* org.pentaho.gwt.filechooser.FileChooser
--- pentaho-gwt-filechooser/src/org/pentaho/gwt/filechooser/client/FileChooserListener.java
+++
@@ -1,8 +0,0 @@
-package org.pentaho.gwt.filechooser.client;
-
-import java.util.EventListener;
-
-public interface FileChooserListener extends EventListener {
-  public void fileSelected(String solution, String path, String name);
-  public void fileSelectionChanged(String solution, String path, String name);
-}
