c1cf800ef9422c886bc2cc7557893e4947ec9f01
Michael D'Amour <mdamour@pentaho.com>
2008-09-18 18:12:49 +0000

    added safety check

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/toolbar/Toolbar.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/toolbar/Toolbar.java
@@ -8,128 +8,132 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.SimplePanel;
 
 /**
- * Displays a collection of buttons in a standard toolbar view. Also supports 
- * ToolbarGroup objects that manage related buttons.
+ * Displays a collection of buttons in a standard toolbar view. Also supports ToolbarGroup objects that manage related buttons.
  * 
  * @author nbaker
  */
-public class Toolbar extends HorizontalPanel{
-  
+public class Toolbar extends HorizontalPanel {
+
   public static final int SEPARATOR = 1;
   public static final int GLUE = 2;
-  
-  //table holding the buttons
+
+  // table holding the buttons
   private HorizontalPanel bar = new HorizontalPanel();
-  
-  //Collection of buttons
+
+  // Collection of buttons
   private List<ToolbarButton> buttons = new ArrayList<ToolbarButton>();
-  
-  //collection of groups
+
+  // collection of groups
   private List<ToolbarGroup> groups = new ArrayList<ToolbarGroup>();
-  
-  public Toolbar(){
-    this.setStylePrimaryName("toolbar");   //$NON-NLS-1$
+
+  public Toolbar() {
+    this.setStylePrimaryName("toolbar"); //$NON-NLS-1$
     this.setVerticalAlignment(ALIGN_MIDDLE);
-    
+
     bar.setVerticalAlignment(ALIGN_MIDDLE);
     bar.setSpacing(1);
     add(bar);
-    
-    setWidth("100%");   //$NON-NLS-1$
-    setHeight("100%");  //$NON-NLS-1$
+
+    setWidth("100%"); //$NON-NLS-1$
+    setHeight("100%"); //$NON-NLS-1$
   }
-  
+
   /**
    * Add in a collection of buttons assembled as a ToolbarGroup
    * 
-   * @param group ToolbarGroup to add.
+   * @param group
+   *          ToolbarGroup to add.
    */
-  public void add(ToolbarGroup group){
-    
-    //check to see if there's already a separator added before this group
-    if( !(bar.getWidget(bar.getWidgetCount()-1) instanceof Image)){
-      bar.add(group.getLeadingSeparator());
-      bar.setCellVerticalAlignment(group.getLeadingSeparator(), ALIGN_MIDDLE);
+  public void add(ToolbarGroup group) {
+
+    // check to see if there's already a separator added before this group
+    if (bar.getWidgetCount() > 0) {
+      if (!(bar.getWidget(bar.getWidgetCount() - 1) instanceof Image)) {
+        bar.add(group.getLeadingSeparator());
+        bar.setCellVerticalAlignment(group.getLeadingSeparator(), ALIGN_MIDDLE);
+      }
     }
-    
-    //if the group has a label tag, add it before the buttons
-    if(group.getLabel() != null){
+
+    // if the group has a label tag, add it before the buttons
+    if (group.getLabel() != null) {
       bar.add(group.getGroupLabel());
       bar.setCellVerticalAlignment(group.getGroupLabel(), ALIGN_MIDDLE);
     }
-    
-    //add the buttons to the bar and buttons collection
-    for(ToolbarButton btn : group.getButtons()){
+
+    // add the buttons to the bar and buttons collection
+    for (ToolbarButton btn : group.getButtons()) {
       bar.add(btn.getPushButton());
     }
     bar.add(group.getTrailingSeparator());
     bar.setCellVerticalAlignment(group.getTrailingSeparator(), ALIGN_MIDDLE);
     groups.add(group);
   }
-  
+
   /**
    * Add a Button to the Toolbar
    */
-  public void add(ToolbarButton button){
+  public void add(ToolbarButton button) {
     bar.add(button.getPushButton());
     buttons.add(button);
   }
-  
+
   /**
    * Add a special element to the toolbar. Support for separator and glue.
-   * @param key id of element to add
+   * 
+   * @param key
+   *          id of element to add
    */
-  public void add(int key){
-    switch(key){
-      case Toolbar.SEPARATOR:
-        Image img = new Image( "style/images/toolbarDivider.png"); //$NON-NLS-1$
-        bar.add(img);
-        bar.setCellVerticalAlignment(img, ALIGN_MIDDLE);
-        break;
-      case Toolbar.GLUE:
-        SimplePanel panel = new SimplePanel();
-        bar.add(panel);
-        bar.setCellWidth(panel, "100%");  //$NON-NLS-1$
-        break;
-      default:
-        //add error logging message
+  public void add(int key) {
+    switch (key) {
+    case Toolbar.SEPARATOR:
+      Image img = new Image("style/images/toolbarDivider.png"); //$NON-NLS-1$
+      bar.add(img);
+      bar.setCellVerticalAlignment(img, ALIGN_MIDDLE);
+      break;
+    case Toolbar.GLUE:
+      SimplePanel panel = new SimplePanel();
+      bar.add(panel);
+      bar.setCellWidth(panel, "100%"); //$NON-NLS-1$
+      break;
+    default:
+      // add error logging message
     }
   }
-  
+
   /**
    * Add a special spacer element to the toolbar.
-   * @param the amount of space to add
+   * 
+   * @param the
+   *          amount of space to add
    */
-  public void addSpacer(int spacerAmount){
+  public void addSpacer(int spacerAmount) {
     SimplePanel panel = new SimplePanel();
     bar.add(panel);
-    bar.setCellWidth(panel, spacerAmount + "px");  //$NON-NLS-1$
+    bar.setCellWidth(panel, spacerAmount + "px"); //$NON-NLS-1$
   }
-  
-  
+
   /**
-   * Enable or Disable the toolbar. If passed in false it will disable all buttons, if true it 
-   * will restore the buttons to their previous state.
+   * Enable or Disable the toolbar. If passed in false it will disable all buttons, if true it will restore the buttons to their previous state.
    * 
-   * @param enabled boolean flag
+   * @param enabled
+   *          boolean flag
    */
-  public void setEnabled(boolean enabled){
-    try{
-      for(ToolbarButton button : this.buttons){
+  public void setEnabled(boolean enabled) {
+    try {
+      for (ToolbarButton button : this.buttons) {
         button.setEnabled(enabled);
         button.setTempDisabled(!enabled);
       }
-      
-      for(ToolbarGroup gp : groups){
+
+      for (ToolbarGroup gp : groups) {
         gp.setEnabled(enabled);
         gp.setTempDisabled(!enabled);
       }
-      
-    } catch(Exception e){
-      System.out.println("Error with Disable: "+e);   //$NON-NLS-1$
+
+    } catch (Exception e) {
+      System.out.println("Error with Disable: " + e); //$NON-NLS-1$
       e.printStackTrace(System.out);
     }
   }
-  
-  
+
 }
