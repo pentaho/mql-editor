fec1ed02c328b2e06cc88d80dd5766825adada59
Nick Baker <nbaker@pentaho.com>
2008-09-19 21:06:39 +0000

    [BISERVER-1974] Toolbar Buttons "blur" to remove the browser added dotted border.

---
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/utils/ElementUtils.java
@@ -0,0 +1,13 @@
+package org.pentaho.gwt.widgets.client.utils;
+
+import com.google.gwt.dom.client.Element;
+
+public class ElementUtils {
+
+  public static native void blur(Element e)/*-{
+   e.blur();
+  }-*/;
+  
+}
+
+  
