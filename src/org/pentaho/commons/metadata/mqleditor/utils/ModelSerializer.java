df89ce1a9bab3e70b9fa1120c3920d52099e007b
Curtis Boyden <cboyden@pentaho.com>
2009-03-05 18:18:02 +0000

    [PPP-1685] Fixed serialization issue

--- src/org/pentaho/commons/metadata/mqleditor/utils/ModelSerializer.java
+++ src/org/pentaho/commons/metadata/mqleditor/utils/ModelSerializer.java
@@ -5,13 +5,15 @@ import org.pentaho.commons.metadata.mqleditor.IQuery;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 
+import org.pentaho.commons.metadata.mqleditor.beans.Query;
+
 
 public class ModelSerializer {
   private static XStream xstreamWriter = new XStream(new JettisonMappedXmlDriver());
-  private static XStream xstreamReader = new XStream(new JettisonMappedXmlDriver());
   static{
     xstreamWriter.setMode(XStream.NO_REFERENCES);
-    xstreamReader.setMode(XStream.NO_REFERENCES);
+
+    xstreamWriter.alias("MQLQuery", Query.class); //$NON-NLS-1$
     
   }
   public static String serialize(IQuery model){
@@ -19,6 +21,6 @@ public class ModelSerializer {
   }
   
   public static IQuery deSerialize(String input){
-    return (IQuery) xstreamReader.fromXML(input);
+    return (IQuery) xstreamWriter.fromXML(input);
   }
 }
