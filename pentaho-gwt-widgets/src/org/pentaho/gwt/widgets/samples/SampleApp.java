271cb2c46f6aa3ef628d018dc7df6f133671f2ac
Nick Baker <nbaker@pentaho.com>
2009-03-13 16:43:01 +0000

    [PPP-1790]
    Updated sample to have one of each type of drop-down

---
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/samples/SampleApp.java
@@ -0,0 +1,84 @@
+package org.pentaho.gwt.widgets.samples;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import org.pentaho.gwt.widgets.client.listbox.CustomListBox;
+import org.pentaho.gwt.widgets.client.listbox.DefaultListItem;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nick Baker
+ * Date: Mar 9, 2009
+ * Time: 12:54:29 PM
+ */
+public class SampleApp implements EntryPoint {
+  public void onModuleLoad() {
+
+    final CustomListBox list = new CustomListBox();
+
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem("Alberta");
+    list.addItem("Atlanta");
+    list.addItem("San Francisco");
+    list.addItem(new DefaultListItem("Testing", new Image("16x16sample.png")));
+    list.addItem(new DefaultListItem("Testing 2", new CheckBox()));
+
+    list.setVisibleRowCount(6);
+
+    list.addChangeListener(new ChangeListener(){
+      public void onChange(Widget widget) {
+        System.out.println(""+list.getSelectedIdex());
+      }
+    });
+
+
+    list.setWidth("150px");
+    //list.setHeight("150px");
+
+    RootPanel.get().add(new Label("Combo: "));
+    RootPanel.get().add(list);
+
+
+    final CustomListBox list2 = new CustomListBox();
+
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem("Alberta");
+    list2.addItem("Atlanta");
+    list2.addItem("San Francisco");
+    list2.addItem(new DefaultListItem("Testing", new Image("16x16sample.png")));
+    list2.addItem(new DefaultListItem("Testing 2", new CheckBox()));
+
+
+    RootPanel.get().add(new Label(""));
+    RootPanel.get().add(new Label("Combo2: "));
+    RootPanel.get().add(list2);
+  }
+}
