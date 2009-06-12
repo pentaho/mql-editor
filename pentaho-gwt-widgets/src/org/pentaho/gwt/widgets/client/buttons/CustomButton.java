02959d4dfc5c151b772688f88377272070d4909e
Ramaiz Mansoor <rmansoor@pentaho.com>
2009-06-12 16:15:30 +0000

    Adding a new button with image and a text

---
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/buttons/CustomButton.java
@@ -0,0 +1,188 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software 
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this 
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html 
+ * or from the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright 2008 Pentaho Corporation.  All rights reserved.
+ */
+package org.pentaho.gwt.widgets.client.buttons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pentaho.gwt.widgets.client.utils.ButtonHelper;
+import org.pentaho.gwt.widgets.client.utils.ElementUtils;
+import org.pentaho.gwt.widgets.client.utils.ButtonHelper.ButtonLabelType;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CustomButton extends Widget {
+
+  private String baseStyleName = "customButton"; //$NON-NLS-1$
+  private Command command;
+  private SimplePanel buttonPanel = new SimplePanel();
+  private boolean enabled = true;
+  private Image image;
+  private String text;
+  private ButtonLabelType type;
+  private List<ClickListener> listeners = new ArrayList<ClickListener>();
+  HorizontalPanel sp;
+  public CustomButton() {
+  }
+
+  public CustomButton(Image image, String text, ButtonLabelType type) {
+    SimplePanel spacer = new SimplePanel();
+    spacer.setWidth("10px");
+    sp = new HorizontalPanel();
+    sp.add(spacer);
+    sp.add(ButtonHelper.createButtonElement(image, text, type));
+    sp.add(spacer);
+    this.setElement(sp.getElement());
+    sp.setStylePrimaryName(baseStyleName);
+    sinkEvents(Event.MOUSEEVENTS);
+    sinkEvents(Event.ONDBLCLICK);    
+  }
+  public CustomButton(Image image, String text, ButtonLabelType type, Command command) {
+    this(image, text, type);
+    setCommand(command);
+  }  
+
+  
+  private void setCommand(Command command) {
+    this.command = command;
+  }
+  
+
+  @Override
+  public void setStylePrimaryName(String style) {
+    super.setStylePrimaryName(style);
+    baseStyleName = style;
+    sp.setStylePrimaryName(style); //$NON-NLS-1$
+  }
+
+  @Override
+  public void addStyleDependentName(String style) {
+    super.addStyleDependentName(style);
+    sp.addStyleDependentName(style);
+  }
+
+  @Override
+  public void removeStyleDependentName(String style) {
+    super.removeStyleDependentName(style);
+    sp.removeStyleDependentName(style);
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public void setEnabled(boolean enabled) {
+    boolean prevVal = this.enabled;
+    this.enabled = enabled;
+
+    if (prevVal && enabled) {
+      return;
+    } else if (prevVal && !enabled) {
+      this.addStyleDependentName("disabled"); //$NON-NLS-1$
+    } else {
+      this.removeStyleDependentName("disabled"); //$NON-NLS-1$
+    }
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void onBrowserEvent(Event event) {
+    switch (event.getTypeInt()) {
+    case Event.ONDBLCLICK:
+    case Event.ONCLICK:
+    case Event.ONMOUSEUP:
+      if (CustomButton.this.isEnabled()) {
+        fireClicked();
+        if (command != null) {
+          command.execute();
+        }
+        event.cancelBubble(true);
+        event.preventDefault();
+      }
+      break;
+    case Event.ONMOUSEOVER:
+      if (CustomButton.this.isEnabled()) {
+        CustomButton.this.addStyleDependentName("hover"); //$NON-NLS-1$
+      }
+      break;
+    case Event.ONMOUSEOUT:
+      if (CustomButton.this.isEnabled()) {
+        CustomButton.this.removeStyleDependentName("hover"); //$NON-NLS-1$
+      }
+      break;
+    case Event.ONMOUSEDOWN:
+      if (CustomButton.this.isEnabled()) {
+        CustomButton.this.addStyleDependentName("down"); //$NON-NLS-1$
+      }
+      break;
+    }
+  }
+
+  private void fireClicked() {
+    for (ClickListener listener : listeners) {
+      listener.onClick(this);
+    }
+  }
+
+  public void addClickListener(ClickListener listener) {
+    listeners.add(listener);
+  }
+
+  public void removeClickListener(ClickListener listener) {
+    listeners.remove(listener);
+  }
+  
+  public void setFocus(boolean focus){
+    this.setFocus(focus);
+  }
+
+  public Image getImage() {
+    return image;
+  }
+
+  public void setImage(Image image) {
+    this.image = image;
+  }
+
+  public ButtonLabelType getType() {
+    return type;
+  }
+
+  public void setType(ButtonLabelType type) {
+    this.type = type;
+  }
+
+  public Command getCommand() {
+    return command;
+  }
+
+
+}
--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/utils/ButtonHelper.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/utils/ButtonHelper.java
@@ -1,10 +1,13 @@
 package org.pentaho.gwt.widgets.client.utils;
 
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public final class ButtonHelper {
@@ -17,7 +20,12 @@ public final class ButtonHelper {
       ButtonLabelType type) {
     return createButtonLabel(image, text, type, null);
   }
-  
+
+  public static Panel createButtonElement(Image image, String text,      
+      ButtonLabelType type) {
+    return createButtonElement(image, text, type, null);
+  }
+
   public static String createButtonLabel(Image img, String text,
       ButtonLabelType type, String cssName) {
     final HTML html = new HTML(text, false);
@@ -61,4 +69,49 @@ public final class ButtonHelper {
       return vpanel.getElement().getString();
     }
   }
+  public static Panel createButtonElement(Image img, String text,
+      ButtonLabelType type, String cssName) {
+    final HTML html = new HTML(text, false);
+    if (cssName != null) {
+      html.addStyleDependentName(cssName);
+      img.addStyleDependentName(cssName);
+    }
+    if (type == ButtonLabelType.TEXT_ONLY) {
+      SimplePanel sp = new SimplePanel();
+      sp.add(html);
+      return sp;
+    } else if (type == ButtonLabelType.TEXT_ON_LEFT
+        || type == ButtonLabelType.TEXT_ON_RIGHT) {
+      HorizontalPanel hpanel = new HorizontalPanel();
+      if (cssName != null) {
+        hpanel.addStyleName(cssName);
+      }
+      if (type == ButtonLabelType.TEXT_ON_LEFT) {
+        hpanel.add(html);
+        hpanel.add(new HTML("&nbsp;"));
+        hpanel.add(img);
+      } else {
+        hpanel.add(img);
+        hpanel.add(new HTML("&nbsp;"));
+        hpanel.add(html);
+      }
+      hpanel.setCellVerticalAlignment(html, HasVerticalAlignment.ALIGN_MIDDLE);
+      hpanel.setCellVerticalAlignment(img, HasVerticalAlignment.ALIGN_MIDDLE);
+      return hpanel;
+    } else {
+      VerticalPanel vpanel = new VerticalPanel();
+      if (type == ButtonLabelType.TEXT_ON_TOP) {
+        vpanel.add(html);
+        vpanel.add(img);
+      } else {
+        vpanel.add(img);
+        vpanel.add(html);
+      }
+      vpanel.setCellHorizontalAlignment(html,
+          HasHorizontalAlignment.ALIGN_CENTER);
+      vpanel.setCellHorizontalAlignment(img,
+          HasHorizontalAlignment.ALIGN_CENTER);
+      return vpanel;
+    }
+  }  
 }
