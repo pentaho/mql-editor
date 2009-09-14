4bc2bf69943b16442cc77228e7dcaee93e6247a9
Michael D'Amour <mdamour@pentaho.com>
2009-09-14 20:52:54 +0000

    BISERVER-3294 Conversion of unicode -> entity/html encoding is not working: Fix is simple, all unicode conversions \uXXXX need to be converted as such &#xXXXX; not &#XXXX;  The leading x ahead of the 4 hex digits is a proper indication that hex digits are to follow, otherwise it is assumed to be decimal, which is not standard.

--- pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/utils/i18n/ResourceBundle.java
+++ pentaho-gwt-widgets/src/org/pentaho/gwt/widgets/client/utils/i18n/ResourceBundle.java
@@ -394,7 +394,7 @@ public class ResourceBundle {
     while (str.indexOf("\\u") != -1) { //$NON-NLS-1$
       int index = str.indexOf("\\u"); //$NON-NLS-1$
       String hex = str.substring(index + 2, index + 6);
-      str = str.substring(0, index) + "&#" + hex + ";" + str.substring(index + 6); //$NON-NLS-1$ //$NON-NLS-2$
+      str = str.substring(0, index) + "&#x" + hex + ";" + str.substring(index + 6); //$NON-NLS-1$ //$NON-NLS-2$
     }
     return str;
   }
