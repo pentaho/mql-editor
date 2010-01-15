003a22897b4a722141ca845b7001e5fe4399dfbd
William Seyler <wseyler@pentaho.com>
2010-01-15 19:51:53 +0000

    agilebi-146: Fixed the deprecations in the SwtMqlEditor by creating a AbstractMqlEditor class that is essentially the old SwingMqlEditor with the Swing classes removed and abstracted in such a way that most of the code is contained in the AbstractMqlEditor.  The Swing and Swt subclasses mearly create Swing or SWT version of XulLoader and XulRunner that are used by the abstract class.

--- pentaho-mql-editor-debug/src/org/pentaho/commons/metadata/mqleditor/DebugSwtMqlEditor.java
+++ pentaho-mql-editor-debug/src/org/pentaho/commons/metadata/mqleditor/DebugSwtMqlEditor.java
@@ -16,27 +16,118 @@
  */
 package org.pentaho.commons.metadata.mqleditor;
 
-import org.pentaho.commons.metadata.mqleditor.editor.SwtMqlEditor;
-import org.pentaho.commons.metadata.mqleditor.editor.service.MQLEditorServiceCWMImpl;
-import org.pentaho.commons.metadata.mqleditor.editor.service.util.CWMStartup;
-import org.pentaho.pms.core.CWM;
-import org.pentaho.pms.factory.CwmSchemaFactory;
-import org.pentaho.pms.schema.SchemaMeta;
+import java.io.InputStream;
 
+import org.pentaho.commons.connection.IPentahoResultSet;
+import org.pentaho.commons.metadata.mqleditor.editor.SwtMqlEditor;
+import org.pentaho.commons.metadata.mqleditor.editor.service.MQLEditorServiceImpl;
+import org.pentaho.commons.metadata.mqleditor.editor.service.util.MQLEditorServiceDelegate;
+import org.pentaho.metadata.query.model.util.QueryXmlHelper;
+import org.pentaho.metadata.repository.FileBasedMetadataDomainRepository;
+import org.pentaho.metadata.repository.IMetadataDomainRepository;
+import org.pentaho.metadata.util.XmiParser;
+import org.pentaho.platform.api.data.IDatasourceService;
+import org.pentaho.platform.api.engine.ISolutionEngine;
+import org.pentaho.platform.api.engine.IPentahoDefinableObjectFactory.Scope;
+import org.pentaho.platform.api.repository.ISolutionRepository;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+import org.pentaho.platform.engine.core.system.StandaloneSession;
+import org.pentaho.platform.engine.services.connection.datasource.dbcp.JndiDatasourceService;
+import org.pentaho.platform.engine.services.solution.SolutionEngine;
+import org.pentaho.platform.plugin.action.pentahometadata.MetadataQueryComponent;
+import org.pentaho.platform.plugin.services.connections.sql.SQLConnection;
+import org.pentaho.platform.repository.solution.filebased.FileBasedSolutionRepository;
+import org.pentaho.test.platform.engine.core.MicroPlatform;
+import org.pentaho.ui.xul.XulServiceCallback;
+
+/**
+ * Default Swt implementation. This class requires a concreate Service
+ * implemetation
+ */
 public class DebugSwtMqlEditor {
 
-  
+
   public static void main(String[] args) {
+
+
+    // initialize micro platorm
+    MicroPlatform microPlatform = new MicroPlatform("resources/solution1/"); //$NON-NLS-1$
+    microPlatform.define(ISolutionEngine.class, SolutionEngine.class);
+    microPlatform.define(ISolutionRepository.class, FileBasedSolutionRepository.class);
+    microPlatform.define(IMetadataDomainRepository.class, FileBasedMetadataDomainRepository.class, Scope.GLOBAL); 
+    microPlatform.define("connection-SQL", SQLConnection.class);  //$NON-NLS-1$
+
+    microPlatform.define(IDatasourceService.class, JndiDatasourceService.class, Scope.GLOBAL);
+    // JNDI
+    System.setProperty("java.naming.factory.initial", "org.osjava.sj.SimpleContextFactory"); //$NON-NLS-1$ //$NON-NLS-2$
+    System.setProperty("org.osjava.sj.root", "resources/solution1/simple-jndi"); //$NON-NLS-1$ //$NON-NLS-2$
+    System.setProperty("org.osjava.sj.delimiter", "/"); //$NON-NLS-1$ //$NON-NLS-2$
     
+    microPlatform.init();
+    new StandaloneSession();
 
-    CWMStartup.loadCWMInstance("/metadata/repository.properties", "/metadata/PentahoCWM.xml"); //$NON-NLS-1$ //$NON-NLS-2$
-    CWM cwm = CWMStartup.loadMetadata("/metadata_steelwheels.xmi", "/metadata"); //$NON-NLS-1$ //$NON-NLS-2$
+    FileBasedMetadataDomainRepository repo = (FileBasedMetadataDomainRepository) PentahoSystem.get(IMetadataDomainRepository.class, null);
+    repo.setDomainFolder("resources/solution1/system/metadata/domains"); //$NON-NLS-1$
+    
+    // Parse and add legacy CWM domain for testing purposes.
+    XmiParser parser = new XmiParser();
+    try {
+      InputStream inStr = SwtMqlEditor.class.getResourceAsStream("/metadata_steelwheels.xmi"); //$NON-NLS-1$
+      if(inStr != null){
+        org.pentaho.metadata.model.Domain d = parser.parseXmi(inStr);
+        d.setId("Steel-Wheels");  //$NON-NLS-1$
+        repo.storeDomain(d, false);
+        repo.reloadDomains();
+        
+      } else {
+        System.out.println("Error loading XMI file");
+       //  System.exit(1);
+      }
+    } catch (Exception e) {
+      System.out.println("error with XMI input"); //$NON-NLS-1$
+    }
+    
+    MQLEditorServiceDelegate delegate = new MQLEditorServiceDelegate(repo) {
+      @Override
+      public String[][] getPreviewData(MqlQuery query, int page, int limit) {
+        org.pentaho.metadata.query.model.Query mqlQuery = convertQueryModel(query);
+        MetadataQueryComponent component = new MetadataQueryComponent();
+        String mqlString = new QueryXmlHelper().toXML(mqlQuery);
+        component.setQuery(mqlString);
+        component.setLive(true);
+        IPentahoResultSet rs = null;
+        try{
+          if (component.execute()) {
+            rs = component.getResultSet();
+            String[][] results = new String[Math.min(rs.getRowCount(), limit)][rs.getColumnCount()];
+            
+            for (int i = 0; i < Math.min(rs.getRowCount(), limit); i++) {
+              for (int j = 0; j < rs.getColumnCount(); j++) {
+                results[i][j] = "" + rs.getValueAt(((page-1) * limit)+i, j); //$NON-NLS-1$
+              }
+            }
+            return results;
+          } else {
+            return null;
+          }
+        } finally{
+          if(rs != null){
+            rs.close();
+          }
+        }
+      }
+    };
 
-    CwmSchemaFactory factory = new CwmSchemaFactory();
-    SchemaMeta meta = factory.getSchemaMeta(cwm);
-
-    SwtMqlEditor editor = new SwtMqlEditor(new MQLEditorServiceCWMImpl(meta), null);
-    editor.hidePreview();
+    MQLEditorServiceImpl service = new MQLEditorServiceImpl(delegate) {
+      @Override
+      public void getPreviewData(MqlQuery query, int page, int limit, XulServiceCallback<String[][]> callback) {
+        callback.success(delegate.getPreviewData(query, page, limit));
+      }
+    };
+    
+    
+    SwtMqlEditor editor = new SwtMqlEditor(repo, service, delegate);
+    //editor.hidePreview();
     editor.show();
   }
   
